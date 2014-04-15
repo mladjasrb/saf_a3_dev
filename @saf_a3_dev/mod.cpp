7621d7e1ba5f901a1c3f76a3f836d7287370c337
mladjasrb <mladja.marinkovicpfc@gmail.com>
2014-04-15 16:48:48 +0200

    Development 0.95.02
    Main Test 1

---
+++ @saf_a3_dev/mod.cpp
@@ -0,0 +1,5 @@
+   name = "Serbian Armed Forces Development (v0.95.01)";
+   picture = "mod.paa";
+   actionName = "Website";
+   action = "http://dev.armasrbija.rs/"; //only websites are currently possible here
+   description = "Serbian Armed Forces is modifications which present modern Serbian Army";
